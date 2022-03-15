597a4be8b91a27747e18f6680f76cd6cf888b08a
marcuscameron <marcus17cameron@gmail.com>
2022-03-14 22:43:30 -0400

    Marcus Cameron

--- scr/Accounts.h
+++ scr/Accounts.h
@@ -11,23 +11,17 @@ using namespace std;
 class Account{
 
 private:
-    string m_firstName;
-    string lastName;
 
-    string m_birthDate;
-    int birthDay;
-    int brithMonth;
-    int brithYear;
+    string accountType;
 
     string m_openDate;
     int accountOpenDay;
     int accountOpenMonth;
     int accountOpenYear;
 
-
     double m_currentBalance;
     int transactionsCount;
-    Transaction[] transactions;
+    Transaction transactions[];
 
 
 
@@ -36,11 +30,7 @@ private:
 public:
     Account();
     Account(string openDate, double balance);
-    void setOpenDate(string openDate){m_openDate = openDate;};
-    void setBalance(double balance){m_balance = balance;};
-    string getOpenDate(){return m_openDate;};
-    double getBalance(){return m_balance;};
-    virtual void print() = 0;
+
 };
 
 
---
+++ scr/BankClient.h
@@ -0,0 +1,32 @@
+//
+// Created by Marcus Cameron on 2022-03-14.
+//
+
+#ifndef BANK_OF_MAC_BANKCLIENT_H
+#define BANK_OF_MAC_BANKCLIENT_H
+
+#include <string>
+using namespace std;
+
+
+class BankClient {
+
+private:
+
+    string m_firstName;
+    string lastName;
+
+    string m_openDate;
+    int accountOpenDay;
+    int accountOpenMonth;
+    int accountOpenYear;
+
+    string m_birthDate;
+    int birthDay;
+    int brithMonth;
+    int brithYear;
+
+};
+
+
+#endif //BANK_OF_MAC_BANKCLIENT_H
