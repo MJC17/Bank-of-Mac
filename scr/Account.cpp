73a4d717537ad4c8a3a764c4337c087a70449d5a
Don <don.martin1002@gmail.com>
2022-03-16 12:13:55 -0400

    Don

--- scr/Account.cpp
+++ scr/Account.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "Account.h"
+#include <iostream>
+using namespace std;
+
+Account::Account()
+{
+    m_accountType = "";
+    m_balance = 0;
+    m_transactionsCount = 0;
+}
+
+Account::Account(string accountType, Date openDate, double balance)
+{
+    m_accountType = accountType;
+    m_openDate = openDate;
+    m_balance = balance;
+    m_transactionsCount = 0;
+}
