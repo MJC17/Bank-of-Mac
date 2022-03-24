97a5c1fe9fb60dcc64e45897c7b1d685103e1f66
marcuscameron <marcus17cameron@gmail.com>
2022-03-24 08:10:12 -0400

    Revert "Matthew Arias"
    This reverts commit a05299ac87a68ed6e507069a7063626d6e5e979c.
    
    # Conflicts:
    #	scr/Account.h

--- scr/Account.h
+++ scr/Account.h
@@ -11,22 +11,27 @@
 
 using namespace std;
 
+
 class Account {
 
 private:
     string accountType;
     Date openDate;
     double balance;
+
     int transactionsCount;
     Transaction transactions[0];
+
 public:
-    Account(string a, double b, Date open);
-    string getAccountType(){return accountType;};
-    void setAccountType(string a){accountType = a;};
+
+    Account(string accountType, double balance);
+
+    Account();
     double getBalance(){return balance;};
-    void setBalance(double b){balance = b;};
-    //virtual void printAccount() = 0;
-    //virtual void printAccountTransactions();
+//
+//    virtual void printAccount();
+//    virtual void printAccountTransactions();
+
 };
 
 #endif //BANK_OF_MAC_ACCOUNT_H
